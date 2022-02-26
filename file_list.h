ed48231288db2e05f1ddd42626a8ed89759e7fee
Maltysen <firnen123@gmail.com>
2022-02-26 01:54:29 -0500

    vim completion help

---
+++ file_list.h
@@ -0,0 +1,10 @@
+// List of file names for vim autocomplete
+int 3dgeometry(){}
+int combo(){}
+int numerical(){}
+int totient(){}
+int amoguslib(){}
+int geometry(){}
+int modular(){}
+int prime(){}
+int constants{}
---
+++ round/.vimrc
@@ -0,0 +1,5 @@
+set path=.,~/programming/cp/amoguslib
+
+set tags=~/programming/cp/amoguslib/tags
+set hidden
+
