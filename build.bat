fbee23a8af71164dcd5c8d732c8a3265afa6da14
Maltysen <firnen123@gmail.com>
2022-02-27 18:06:57 -0500

    more windows fix

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@ g++ --std=gnu++14 -I%~dp0 -g -O3 -Wall -Wshadow -Wno-sign-compare %1.cpp -o %1 2
 
 if not exist "out" mkdir "out"
 if exist out\%1out.cpp del out\%1out.cpp
-echo "#include <bits/stdc++.h>" >> out\%1out.cpp
+echo|set /p="#include <bits/stdc++.h>" > out\%1out.cpp
+echo. >> out\%1out.cpp
 
 cpp -I%~dp0 %1.cpp -DCOPIER  >> out\%1out.cpp
