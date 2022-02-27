8216b463c7a01dd9ba9e39b46dcd6618d18fc85d
Maltysen <firnen123@gmail.com>
2022-02-27 09:34:19 -0500

    fixed pbds

--- pbds.h
+++ pbds.h
@@ -1,11 +1,15 @@
 #pragma once
 
-#include <bits/extc++.h>
-using namespace __gnu_pbds;
+#ifndef COPIER
+    #include <bits/extc++.h>
+#else
+    TEMP_INCLUDE_EXT
+#endif
 
+using namespace __gnu_pbds;
 
 template <class T>
 using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-template <class K, V>
+template <class K, class V>
 using o_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
