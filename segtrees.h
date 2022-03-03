c56866aa246f5beebfb266f577e3dd5048b5af63
Maltysen <firnen123@gmail.com>
2022-03-02 22:42:23 -0500

    faster lazy segtree

--- segtrees.h
+++ segtrees.h
@@ -45,7 +45,8 @@ struct lazy_segtree {
     }
 
     void calc(int p) {
-        t[p] = h(d[p], f(t[p*2], t[p*2+1]));
+        t[p] = f(t[p*2], t[p*2+1]);
+        if (d[p]!=idU) t[p] = h(d[p], t[p]);
     }
 
     void apply(int p, U v) {
@@ -60,10 +61,11 @@ struct lazy_segtree {
         }
     }
 
-    void push(int l, int r) {
-        int s=L;
-        for (l+=n, r+=n-1; s>0; --s) {
-            for (int i=l>>s; i<=r>>s; ++i) if (d[i]!=idU) {
+    void push(int p) {
+        p+=n;
+        for (int s=L; s>0; --s) {
+            int i=p>>s;
+            if (d[i]!=idU) {
                 apply(i*2, d[i]);
                 apply(i*2+1, d[i]);
                 d[i]=idU;
@@ -72,27 +74,29 @@ struct lazy_segtree {
     }
 
     void modify(int p, T v) {
-        push(p, p+1);
+        push(p);
         t[p+n] = v;
         build(p, p+1);
     }
 
     void modify(int l, int r, U v) {
         if (v==idU) return;
-        push(l, l+1);
-        push(r-1, r);
-        int l0=l, r0=r;
+        push(l), push(r-1);
+        bool cl=false, cr=false;
         for (l+=n, r+=n; l<r; l/=2, r/=2) {
-            if (l&1) apply(l++, v);
-            if (r&1) apply(--r, v);
+            if (cl) calc(l-1);
+            if (cr) calc(r);
+            if (l&1) apply(l++, v), cl=true;
+            if (r&1) apply(--r, v), cr=true;
         }
-        build(l0, l0+1);
-        build(r0-1, r0);
+        for (--l; r>0; l/=2, r/=2) {
+            if (cl) calc(l);
+            if (cr && (!cl || l != r)) calc(r);
+        }
     }
 
     T query(int l, int r) {
-        push(l, l+1);
-        push(r-1, r);
+        push(l), push(r-1);
         T resl=idT, resr=idT;
         for (l += n, r += n; l < r; l/=2, r/=2) {
             if (l&1) resl = f(resl, t[l++]);
